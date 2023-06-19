837ede2ead41b5242f2d0d906a006e8c2aabd285
SzakacsAgi <61914845+SzakacsAgi@users.noreply.github.com>
2023-06-19 18:55:34 +0200

    Update BlogiApplication.java

--- src/main/java/com/university/thesis/blogi/blogifrontend/BlogiApplication.java
+++ src/main/java/com/university/thesis/blogi/blogifrontend/BlogiApplication.java
@@ -1,2 +1,13 @@
-package com.university.thesis.blogi.blogifrontend;public class BlogiApplication {
+package com.university.thesis.blogi.blogifrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogiApplication.class, args);
+    }
+
 }
