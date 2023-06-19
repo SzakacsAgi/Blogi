57a0a720174cd9fd01d23cfadd02e92c2699681c
SzakacsAgi <szakacs.agi21@gmail.com>
2023-06-19 19:33:49 +0200

    Add dummy page loader controller to the project

--- src/main/java/com/university/thesis/blogi/frontend/web/pageloader/controller/DummyHTMLPageLoaderController.java
+++ src/main/java/com/university/thesis/blogi/frontend/web/pageloader/controller/DummyHTMLPageLoaderController.java
@@ -1,4 +1,4 @@
-package web.pageloader.controller;
+package com.university.thesis.blogi.frontend.web.pageloader.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
