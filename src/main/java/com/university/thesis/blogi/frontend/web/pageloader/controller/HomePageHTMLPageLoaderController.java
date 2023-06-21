250b44120d4f7b46ffda4ed9eb7382e9f0dba27e
SzakacsAgi <61914845+SzakacsAgi@users.noreply.github.com>
2023-06-21 21:52:12 +0200

    Add Page Loader Controller for the home page (#5)

---
+++ src/main/java/com/university/thesis/blogi/frontend/web/pageloader/controller/HomePageHTMLPageLoaderController.java
@@ -0,0 +1,15 @@
+package com.university.thesis.blogi.frontend.web.pageloader.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomePageHTMLPageLoaderController {
+
+    private static final String HTML_PAGE_VIEW_NAME = "home-page";
+
+    @GetMapping(path = "/home")
+    public ModelAndView loadPage() {
+        return new ModelAndView(HTML_PAGE_VIEW_NAME);
+    }
+}
