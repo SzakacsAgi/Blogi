0e7b3f362e7555e1d95ba2b54b480f8b2249a50d
bob <bob@>
2023-08-02 12:26:20 +0200

    Add Page Loader Controller for the new article page

--- src/main/java/com/university/thesis/blogi/frontend/web/pageloader/controller/NewArticleHTMLPageLoaderController.java
+++ src/main/java/com/university/thesis/blogi/frontend/web/pageloader/controller/NewArticleHTMLPageLoaderController.java
@@ -8,7 +8,7 @@ public class NewArticleHTMLPageLoaderController {
 
     private static final String HTML_PAGE_VIEW_NAME = "new-article-page";
 
-    @GetMapping(path = "/test/new")
+    @GetMapping(path = "/new-article")
     public ModelAndView loadPage() {
         return new ModelAndView(HTML_PAGE_VIEW_NAME);
     }
