fefeb28bd060e9fcd54989e98d9cb1ce81394950
bob <bob@>
2023-10-02 18:38:20 +0200

    Fix some styling problem

--- src/main/resources/js/browser/browser-data-modifier.js
+++ src/main/resources/js/browser/browser-data-modifier.js
@@ -3,6 +3,7 @@ class BrowserDataModifier {
     constructor() { }
 
     updateTitleWith(title){
+        title = title.charAt(0).toUpperCase() + title.slice(1);
         document.title = title;
     }
 }
